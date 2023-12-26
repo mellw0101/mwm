5f668302c99bee4fb5e91680e3d44aaf239289a6
mellw0101 <melwivensson@gmail.com>
2023-12-26 18:23:30 +0100

    Changes to be committed: 	modified:   mwm/Makefile 	modified:   mwm/install_depends.sh

--- mwm/install_depends.sh
+++ mwm/install_depends.sh
@@ -6,15 +6,80 @@ install_dependencies()
     if [ -x "$(command -v apt)" ]; then
         # Debian/Ubuntu
         sudo apt update
-        sudo apt install -y libpng++-dev
+        sudo apt install -y libpng++-dev            \
+                            libxcb1-dev             \
+                            libxcb-xvmc0-dev        \
+                            libxcb-xvmc0-dev        \
+                            libxcb-xv0-dev          \
+                            libxcb-xtest0-dev       \
+                            libxcb-shm0-dev         \
+                            libxcb-randr0-dev       \
+                            libxcb-image0-dev       \
+                            libxcb-keysyms1-dev     \
+                            libxcb-icccm4-dev       \
+                            libxcb-sync0-dev        \
+                            libxcb-xinerama0-dev    \
+                            libxcb-xfixes0-dev      \
+                            libxcb-shape0-dev       \
+                            libxcb-present-dev      \
+                            libxcb-dri3-dev         \
+                            libxcb-dri2-0-dev       \
+                            libxcb-damage0-dev      \
+                            libxcb-composite0-dev   \
+                            libxcb-cursor-dev       \
+                            libxcb-xkb-dev          \
+                            libxkbcommon-dev        \
+                            libxkbcommon-x11-dev    \
+                            libxkbfile-dev          \
+                            libx11-xcb-dev          \
+                            libx11-dev              \
+                            libxext-dev             \
+                            libxfixes-dev           \
+                            libxdamage-dev          \
+                            libxrender-dev          \
+                            libxcomposite-dev       \
+                            libxinerama-dev         \
+                            libxrandr-dev           \
+                            libxss-dev              \
+                            libxtst-dev             \
+                            libxv-dev               \
+                            libxvmc-dev             \
+                            libxshmfence-dev        \
+                            libxxf86vm-dev
     elif [ -x "$(command -v dnf)" ]; then
         # Fedora/RHEL
         sudo dnf install -y package1 package2
     elif [ -x "$(command -v pacman)" ]; then
         # Arch Linux
-        sudo pacman -Syu --noconfirm package1 package2
+        sudo pacman -Syu --noconfirm    xcb-proto           \
+                                        libxcb              \
+                                        xcb-util            \
+                                        xcb-util-image      \
+                                        xcb-util-keysyms    \
+                                        xcb-util-renderutil \
+                                        xcb-util-wm         \
+                                        xcb-util-cursor     \
+                                        libxkbcommon        \
+                                        libxkbcommon-x11    \
+                                        libxkbfile          \
+                                        libx11              \
+                                        libxext             \
+                                        libxfixes           \
+                                        libxdamage          \
+                                        libxrender          \
+                                        libxcomposite       \
+                                        libxinerama         \
+                                        libxrandr           \
+                                        libxss              \
+                                        libxtst             \
+                                        libxv               \
+                                        libxvmc             \
+                                        libxshmfence        \
+                                        libxxf86vm                        
     else
         echo "Unsupported distribution. Please install dependencies manually."
         exit 1
     fi
 }
+
+install_dependencies
