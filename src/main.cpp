7ad62d9fde2a9707983d0f8ee01219caffa472fe
mellw0101 <melwivensson@gmail.com>
2023-12-31 19:26:08 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2741,7 +2741,7 @@ class change_desktop
                         {
                             // animate_client(c, c->x - screen->width_in_pixels, c->y, c->width, c->height, 1000);
 
-                            t = std::thread(animate_client, c, c->x - screen->width_in_pixels, c->y, c->width, c->height, 1000);
+                            t = std::thread(animate_client, this, c, c->x - screen->width_in_pixels, c->y, c->width, c->height, 1000);
                             t.detach();
                         }
                     }
