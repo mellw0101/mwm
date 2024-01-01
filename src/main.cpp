3082b8d72f9ce889fbc2e4da8b34940c38d22bb8
mellw0101 <melwivensson@gmail.com>
2024-01-01 20:46:38 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7191,7 +7191,8 @@ configureRootWindow()
             XCB_EVENT_MASK_BUTTON_RELEASE               |
             XCB_EVENT_MASK_KEY_PRESS                    |
             XCB_EVENT_MASK_FOCUS_CHANGE                 |
-            XCB_EVENT_MASK_KEY_RELEASE
+            XCB_EVENT_MASK_KEY_RELEASE                  |
+            XCB_EVENT_MASK_POINTER_MOTION
         }
     );
 
