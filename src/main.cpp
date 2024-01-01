ebc753b23da2b259c9cda8499f6afbbd5c1081a6
mellw0101 <melwivensson@gmail.com>
2024-01-01 01:01:19 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3835,7 +3835,7 @@ namespace win_tools
         kill_client(conn, c->frame);
         xcb_flush(conn);
         
-        removeClient(c->win);
+        delete c;
 
         return 0;
     }
@@ -5965,7 +5965,7 @@ class Event
             WinManager::kill_client(conn, c->titlebar);
             WinManager::kill_client(conn, c->frame);
             xcb_flush(conn);
-            removeClient(c->win);
+            delete c;
         }
 
         void
