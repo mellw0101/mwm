1a7fc901d4d84f7221747b54e7d68bdb1281a22e
mellw0101 <melwivensson@gmail.com>
2024-01-11 12:42:33 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -929,7 +929,7 @@ class mxb
                         make_apps()
                         {
                             buttons.add("test", nullptr);
-                            buttons.list[0].create(main_window, 0, 0, width, height, GREEN);
+                            buttons.list[0].create(main_window, 2, 2, width - 4, height - 4, GREEN);
                         }
                     ;
                 };
