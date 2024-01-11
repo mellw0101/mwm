41b8f98047546dec854397c1ce0e09f9cc2ce350
mellw0101 <melwivensson@gmail.com>
2024-01-11 11:24:52 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -820,8 +820,17 @@ class mxb
                         setup_dock()
                         {
                             uint32_t mask = 
-                                XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY |
-                                XCB_EVENT_MASK_POINTER_MOTION
+                                XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
+                                XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY   |
+                                XCB_EVENT_MASK_ENTER_WINDOW          |
+                                XCB_EVENT_MASK_LEAVE_WINDOW          |
+                                XCB_EVENT_MASK_STRUCTURE_NOTIFY      |
+                                XCB_EVENT_MASK_BUTTON_PRESS          |
+                                XCB_EVENT_MASK_BUTTON_RELEASE        |
+                                XCB_EVENT_MASK_KEY_PRESS             |
+                                XCB_EVENT_MASK_FOCUS_CHANGE          |
+                                XCB_EVENT_MASK_KEY_RELEASE           |
+                                XCB_EVENT_MASK_POINTER_MOTION 
                             ;
                             main_window.apply_event_mask(& mask);
                             main_window.grab_button({{R_MOUSE_BUTTON, NULL}});
