bf82b76064e90bb82784cc28831bece77214814c
mellw0101 <melwivensson@gmail.com>
2024-01-12 12:04:57 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9569,19 +9569,35 @@ configureRootWindow()
     // mxb::set::win::backround::as_color(screen->root, DARK_GREY);
     root.set_backround_color(DARK_GREY);
 
-    uint32_t mask = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
-                    XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY   |
-                    XCB_EVENT_MASK_ENTER_WINDOW          |
-                    XCB_EVENT_MASK_LEAVE_WINDOW          |
-                    XCB_EVENT_MASK_STRUCTURE_NOTIFY      |
-                    XCB_EVENT_MASK_BUTTON_PRESS          |
-                    XCB_EVENT_MASK_BUTTON_RELEASE        |
-                    XCB_EVENT_MASK_KEY_PRESS             |
-                    XCB_EVENT_MASK_FOCUS_CHANGE          |
-                    XCB_EVENT_MASK_KEY_RELEASE           |
-                    XCB_EVENT_MASK_POINTER_MOTION
-    ;
-    mxb::set::event_mask(& mask, screen->root);
+    // uint32_t mask = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
+    //                 XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY   |
+    //                 XCB_EVENT_MASK_ENTER_WINDOW          |
+    //                 XCB_EVENT_MASK_LEAVE_WINDOW          |
+    //                 XCB_EVENT_MASK_STRUCTURE_NOTIFY      |
+    //                 XCB_EVENT_MASK_BUTTON_PRESS          |
+    //                 XCB_EVENT_MASK_BUTTON_RELEASE        |
+    //                 XCB_EVENT_MASK_KEY_PRESS             |
+    //                 XCB_EVENT_MASK_FOCUS_CHANGE          |
+    //                 XCB_EVENT_MASK_KEY_RELEASE           |
+    //                 XCB_EVENT_MASK_POINTER_MOTION
+    // ;
+    // mxb::set::event_mask(& mask, screen->root);
+    root.apply_event_mask
+    (
+        {
+            XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT,
+            XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY,
+            XCB_EVENT_MASK_ENTER_WINDOW,
+            XCB_EVENT_MASK_LEAVE_WINDOW,
+            XCB_EVENT_MASK_STRUCTURE_NOTIFY,
+            XCB_EVENT_MASK_BUTTON_PRESS,
+            XCB_EVENT_MASK_BUTTON_RELEASE,
+            XCB_EVENT_MASK_KEY_PRESS,
+            XCB_EVENT_MASK_FOCUS_CHANGE,
+            XCB_EVENT_MASK_KEY_RELEASE,
+            XCB_EVENT_MASK_POINTER_MOTION
+        }
+    );
     mxb::win::clear(screen->root);
 
     // FLUSH TO MAKE X SERVER HANDEL REQUEST NOW
