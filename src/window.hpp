5335aee6834d661782eceb1058f0734491dd8cc3
mellw0101 <melwivensson@gmail.com>
2024-01-13 23:05:05 +0100

    Changes to be committed: 	modified:   src/window.hpp

--- src/window.hpp
+++ src/window.hpp
@@ -46,59 +46,6 @@
 #include <arpa/inet.h>
 #include <thread>
 #include <atomic>
- #include <cstdlib>
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <string.h>
-#include <utility>
-#include <vector>
-#include <xcb/randr.h>
-#include <xcb/xcb_keysyms.h>
-#include <xcb/xcb_icccm.h>
-#include <xcb/xcb_ewmh.h>
-#include <xcb/xcb_xrm.h>
-#include <xcb/render.h>
-#include <X11/keysym.h>
-#include <X11/X.h>
-#include <cstddef>
-#include <cstdint>
-#include <cstring>
-#include <xcb/xcb.h>
-#include <unistd.h>     // For fork() and exec()
-#include <sys/wait.h>   // For waitpid()
-#include <xcb/xcb_ewmh.h>
-#include <xcb/xcb_keysyms.h>
-#include <xcb/xproto.h>
-#include <xcb/xcb_cursor.h> /* For cursor */
-#include <xcb/xcb_icccm.h>
-#include <spawn.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <iostream>
-#include <thread>
-#include <chrono>
-#include <atomic>
-#include <mutex> // Added for thread safety
-#include <functional>
-#include <iostream>
-#include <algorithm>
-#include <png.h>
-#include <xcb/xcb_image.h>
-#include <Imlib2.h>
-#include <stdexcept>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <unistd.h>
-#include <X11/Xauth.h>
-#include <string>
-#include <cstring>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <map>
 
 #include "Log.hpp"
 #include "defenitions.hpp"
