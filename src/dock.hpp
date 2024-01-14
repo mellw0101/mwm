96789490b0a50423c8e2292f2b77b7f709b68d3e
mellw0101 <melwivensson@gmail.com>
2024-01-15 00:18:52 +0100

    Changes to be committed: 	modified:   src/dock.hpp

--- src/dock.hpp
+++ src/dock.hpp
@@ -125,7 +125,7 @@ class add_app_dialog_window
             wm->event_handler.setEventCallback(XCB_CONFIGURE_NOTIFY, [&](Ev ev) 
             {
                 const auto * e = reinterpret_cast<const xcb_configure_notify_event_t *>(ev);
-                if (e->window == main_window)
+                if (e->event == main_window)
                 {
                     search_window.width((e->width - (DOCK_BORDER * 2)));
                 }
