11494ef2de7a365988fee1fd85881e3caa69e7f2
mellw0101 <melwivensson@gmail.com>
2024-01-14 22:02:16 +0100

    Changes to be committed: 	modified:   src/dock.hpp

--- src/dock.hpp
+++ src/dock.hpp
@@ -76,7 +76,7 @@ class add_app_dialog_window
         {
             window.unmap();
             window.kill();
-            wm->send_sigterm_to_client(c);
+            c->unmap();
         }
     ;
 };
