46dc77bbb756f8a0f1dbabe74ba410886631b9ff
mellw0101 <melwivensson@gmail.com>
2024-01-14 16:05:09 +0100

    Changes to be committed: 	modified:   src/str.hpp

--- src/str.hpp
+++ src/str.hpp
@@ -173,7 +173,6 @@ class str
                 length = other.length;
                 data = new char[length + 1];
                 strcpy(data, other.data);
-                is_null = false;
             }
             return *this;
         }
@@ -188,7 +187,14 @@ class str
                 length = other.length;
                 other.data = nullptr;
                 other.length = 0;
-                is_null = bool(data == nullptr);
+                if (data == nullptr)
+                {
+                    is_null = true;
+                }
+                else
+                {
+                    is_null = false;
+                }
             }
             return *this;
         }
