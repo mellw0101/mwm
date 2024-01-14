794dbcba87d00cd2aad40d0a91a848bf70915b12
mellw0101 <melwivensson@gmail.com>
2024-01-14 23:22:43 +0100

    Changes to be committed: 	modified:   src/dock.hpp

--- src/dock.hpp
+++ src/dock.hpp
@@ -47,6 +47,7 @@ class add_app_dialog_window
             {
                 c->x_y((pointer.x() - (window.width() / 2)), (pointer.y() - (window.height() / 2)));
                 c->map();
+                return;
             }
 
             create_client();
