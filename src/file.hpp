3d44ff025db93655f40ec67a5ea810bdd49509ed
mellw0101 <melwivensson@gmail.com>
2024-01-14 16:17:15 +0100

    Changes to be committed: 	modified:   src/file.hpp

--- src/file.hpp
+++ src/file.hpp
@@ -105,7 +105,7 @@ class File
         std::vector<const char *>
         split_$PATH_into_vector()
         {
-            str $PATH = get_env_var("PATH");
+            str $PATH(get_env_var("PATH"));
             return st.tokenize($PATH.c_str(), ":");
         }
 
