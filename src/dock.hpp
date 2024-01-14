3538c3938c569aad9a09c4eee4f4cf605d8f29bc
mellw0101 <melwivensson@gmail.com>
2024-01-14 19:39:16 +0100

    Changes to be committed: 	modified:   src/dock.hpp 	new file:   src/event_handler.hpp

--- src/dock.hpp
+++ src/dock.hpp
@@ -7,7 +7,6 @@
 #include <sys/types.h>
 #include <xcb/xcb.h>
 #include <functional>
-#include <unordered_map>
 #include <xcb/xproto.h>
 
 #include "Log.hpp"
@@ -18,53 +17,7 @@
 #include "pointer.hpp"
 #include "structs.hpp"
 #include "window.hpp"
-
-class Event_Handler
-{
-    public:
-        using EventCallback = std::function<void(const xcb_generic_event_t*)>;
-
-        void 
-        run() 
-        {
-            xcb_generic_event_t *ev;
-            shouldContinue = true;
-
-            while (shouldContinue) 
-            {
-                ev = xcb_wait_for_event(conn);
-                if (!ev) 
-                {
-                    continue;
-                }
-
-                uint8_t responseType = ev->response_type & ~0x80;
-                if (eventCallbacks.find(responseType) != eventCallbacks.end()) 
-                {
-                    eventCallbacks[responseType](ev);
-                }
-
-                free(ev);
-            }
-        }
-
-        void
-        end()
-        {
-            shouldContinue = false;
-        }
-
-        void 
-        setEventCallback(uint8_t eventType, EventCallback callback) 
-        {
-            eventCallbacks[eventType] = std::move(callback);
-        }
-    ;
-    private:
-        std::unordered_map<uint8_t, EventCallback> eventCallbacks;
-        bool shouldContinue = false;
-    ;
-};
+#include "event_handler.hpp"
 
 class add_app_dialog_window
 {
---
+++ src/event_handler.hpp
@@ -0,0 +1,58 @@
+#ifndef EVENT_HANDLER_HPP
+#define EVENT_HANDLER_HPP
+
+#include <xcb/xcb.h>
+#include <functional>
+#include <unordered_map>
+#include <xcb/xproto.h>
+
+#include "structs.hpp"
+
+class Event_Handler
+{
+    public:
+        using EventCallback = std::function<void(const xcb_generic_event_t*)>;
+
+        void 
+        run() 
+        {
+            xcb_generic_event_t *ev;
+            shouldContinue = true;
+
+            while (shouldContinue) 
+            {
+                ev = xcb_wait_for_event(conn);
+                if (!ev) 
+                {
+                    continue;
+                }
+
+                uint8_t responseType = ev->response_type & ~0x80;
+                if (eventCallbacks.find(responseType) != eventCallbacks.end()) 
+                {
+                    eventCallbacks[responseType](ev);
+                }
+
+                free(ev);
+            }
+        }
+
+        void
+        end()
+        {
+            shouldContinue = false;
+        }
+
+        void 
+        setEventCallback(uint8_t eventType, EventCallback callback) 
+        {
+            eventCallbacks[eventType] = std::move(callback);
+        }
+    ;
+    private:
+        std::unordered_map<uint8_t, EventCallback> eventCallbacks;
+        bool shouldContinue = false;
+    ;
+};
+
+#endif // EVENT_HANDLER_HPP
