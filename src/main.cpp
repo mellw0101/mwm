926d4b7607df9523509f6ff0dc3b538f737f64dc
mellw0101 <melwivensson@gmail.com>
2024-01-14 20:47:46 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4473,8 +4473,10 @@ class Event
         map_notify_handler(const xcb_generic_event_t * & ev)
         {
             const auto * e = reinterpret_cast<const xcb_map_notify_event_t *>(ev);
-            log_win("e->window: ", e->window);
-            log_win("e->window: ", e->event);
+            if (e->window == dock->add_app_dialog_window.window)
+            {
+                wm->manage_new_client(e->window);
+            }
             client * c = wm->client_from_window(& e->window);
             if (c)
             {
