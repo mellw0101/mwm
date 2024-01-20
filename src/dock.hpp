789faa2dedaa185b52d7ec4842fdbd72ca23b742
mellw0101 <melwivensson@gmail.com>
2024-01-20 19:25:12 +0100

    Changes to be committed: 	modified:   src/dock.hpp

--- src/dock.hpp
+++ src/dock.hpp
@@ -505,10 +505,7 @@ class File_App {
             });
             event_handler->setEventCallback(XCB_CONFIGURE_NOTIFY, [&](Ev ev) {
                 const auto * e = reinterpret_cast<const xcb_configure_notify_event_t *>(ev);
-                if (e->window == main_window)
-                {
-                    log_info("success");
-                }
+                log_info("success");
             });
         }
     ;
