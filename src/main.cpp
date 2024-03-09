1dd84fbb173fb24dceb1f714ffec18841ff975a3
mellw0101 <melwivensson@gmail.com>
2024-03-09 23:48:23 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10690,8 +10690,6 @@ class Events
                 c->win.y(TITLE_BAR_HEIGHT + BORDER_SIZE);
                 xcb_flush(conn);
             }
-
-            xcb_flush(conn);
         }
 
         void enter_notify_handler(const xcb_generic_event_t *&ev)
