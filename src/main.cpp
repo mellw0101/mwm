d8fcdf86b33e9804bd8b5882c4726984bb3f7378
mellw0101 <melwivensson@gmail.com>
2024-03-09 17:24:53 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4908,21 +4908,29 @@ class Window_Manager
                 root.set_backround_color(DARK_GREY);
                 uint32_t mask = 
                     XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
-                    XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY |
-                    XCB_EVENT_MASK_ENTER_WINDOW |
-                    XCB_EVENT_MASK_LEAVE_WINDOW |
-                    XCB_EVENT_MASK_STRUCTURE_NOTIFY |
-                    XCB_EVENT_MASK_BUTTON_PRESS |
-                    XCB_EVENT_MASK_BUTTON_RELEASE |
-                    XCB_EVENT_MASK_KEY_PRESS |
-                    XCB_EVENT_MASK_KEY_RELEASE |
-                    XCB_EVENT_MASK_FOCUS_CHANGE |
+                    XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY   |
+                    XCB_EVENT_MASK_ENTER_WINDOW          |
+                    XCB_EVENT_MASK_LEAVE_WINDOW          |
+                    XCB_EVENT_MASK_STRUCTURE_NOTIFY      |
+                    XCB_EVENT_MASK_BUTTON_PRESS          |
+                    XCB_EVENT_MASK_BUTTON_RELEASE        |
+                    XCB_EVENT_MASK_KEY_PRESS             |
+                    XCB_EVENT_MASK_KEY_RELEASE           |
+                    XCB_EVENT_MASK_FOCUS_CHANGE          |
                     XCB_EVENT_MASK_POINTER_MOTION
                 ; 
-                root.apply_event_mask(& mask);
+                root.apply_event_mask(&mask);
                 root.clear();
 
-                root.set_backround_png("/home/mellw/mwm_png/galaxy17.png");
+                if (root.width() == 3840 && root.height() == 1200)
+                {
+                    root.set_backround_png("/home/mellw/mwm_png/galaxy16-17-3840x1200.png");
+                }
+                else
+                {
+                    root.set_backround_png("/home/mellw/mwm_png/galaxy17.png");
+                }
+
                 root.set_pointer(CURSOR::arrow);
             }
         
