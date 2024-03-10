bd7294c9487fc031018a702bc5001c8e99e31c5f
mellw0101 <melwivensson@gmail.com>
2024-03-10 20:55:23 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8395,11 +8395,12 @@ class __system_settings__
                 expose(e->window);
             });
 
-            event_handler->setEventCallback(XCB_EVENT_MASK_NO_EVENT, [this](Ev ev)-> void
+            event_handler->setEventCallback(XCB_CLIENT_MESSAGE, [this](Ev ev)-> void
             {
                 auto e = reinterpret_cast<const xcb_client_message_event_t *>(ev);
                 if (e->window == c->win)
                 {
+                    NET_LOG("win detected");
                     if (e->format == 32)
                     {
                         xcb_atom_t atom;
