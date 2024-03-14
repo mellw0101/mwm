15786a9dc4659cd9446a9f2ed8d765a7e5f30ed1
mellw0101 <melwivensson@gmail.com>
2024-03-14 14:10:25 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8799,7 +8799,7 @@ class __system_settings__
             {
                 // string resolution(screen_settings->_current_resoluton_string);
                 string resolution("Balle");
-                RETURN_IF(resolution.empty())
+                // RETURN_IF(resolution.empty())
                 _screen_resolution_window.draw_text(
                     resolution.c_str(),
                     WHITE,
