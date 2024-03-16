25e4f8026b749eb77a24966841e71b2cdb1f8b66
mellw0101 <melwivensson@gmail.com>
2024-03-16 20:51:07 +0100

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8594,7 +8594,9 @@ class __system_settings__
                 160,
                 20,
                 DARK_GREY,
-                XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS
+                XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS,
+                NONE,
+                (int[]){ALL, 2, BLACK}
             );
 
             _screen_resolution_button_window.create_window(
@@ -8604,7 +8606,9 @@ class __system_settings__
                 20,
                 20,
                 DARK_GREY,
-                XCB_EVENT_MASK_BUTTON_PRESS
+                XCB_EVENT_MASK_BUTTON_PRESS,
+                NONE,
+                (int[]){RIGHT | DOWN | UP, 2, BLACK}
             );
 
             _audio_menu_entry_window.create_window(
@@ -8719,10 +8723,10 @@ class __system_settings__
             {
                 expose(__window);
                 _screen_resolution_window.map();
-                _screen_resolution_window.make_borders(ALL, 2, BLACK);
+                // _screen_resolution_window.make_borders(ALL, 2, BLACK);
                 expose(_screen_resolution_window);
                 _screen_resolution_button_window.map();
-                _screen_resolution_button_window.make_borders(UP | RIGHT | DOWN, 2, BLACK);
+                // _screen_resolution_button_window.make_borders(UP | RIGHT | DOWN, 2, BLACK);
             }
 
             if (__window == _input_settings_window)
