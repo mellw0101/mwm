3d1e62e98c89057e669dbfd7c643b1917d0e57ca
mellw0101 <melwivensson@gmail.com>
2024-03-18 00:10:49 +0000

    Changes to be committed: 	new file:   .vscode/c_cpp_properties.json 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3905,8 +3905,8 @@ class client
             {
                 titlebar.clear();
 
-                if (__mode & TITLE_REQ_DRAW ) titlebar.draw_text_16_auto_color(win.get_net_wm_name_by_req().c_str(), 4, 15);
-                if (__mode & TITLE_INTR_DRAW) titlebar.draw_text_16_auto_color(win.get_net_wm_name().c_str(), 4, 15);
+                if (__mode & TITLE_REQ_DRAW ) titlebar.draw_text_16_auto_color(win.get_net_wm_name_by_req().c_str(), ((width / 2) - ((win.get_net_wm_name_by_req().length() * DEFAULT_FONT_WIDTH) / 2)), 15);
+                if (__mode & TITLE_INTR_DRAW) titlebar.draw_text_16_auto_color(win.get_net_wm_name().c_str(), ((width / 2) - ((win.get_net_wm_name().length() * DEFAULT_FONT_WIDTH) / 2)), 15);
             }
         
         // Config.
@@ -4137,7 +4137,7 @@ class client
                 win.unset_EWMH_fullscreen_state();
             }
         
-    private:
+private:
     // Methods.
         void make_frame()
         {
