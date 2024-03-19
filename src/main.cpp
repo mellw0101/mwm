1ac2a16ff4fc3a2bbbb015f66ea1d71eceb5b57d
mellw0101 <melwivensson@gmail.com>
2024-03-19 11:41:14 +0000

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5264,6 +5264,7 @@ class Window_Manager {
                 // ; 
                 // root.apply_event_mask(&mask);
                 root.set_event_mask(ROOT_EVENT_MASK);
+                root.grab_default_keys();
                 root.clear();
                 root.set_backround_png(USER_PATH_PREFIX_C_STR("/mwm_png/galaxy21.png"));
                 root.set_pointer(CURSOR::arrow);
