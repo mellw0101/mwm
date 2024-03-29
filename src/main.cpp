373f129562fca34b9164e6db4cd5e6580d9f8a87
mellw0101 <melwivensson@gmail.com>
2024-03-29 18:39:37 +0000

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6827,7 +6827,7 @@ class context_menu {
                 entries[i].window.setup_WIN_SIG(L_MOUSE_BUTTON_PRESS, [this, i]() -> void { if (entries[i].action) entries[i].action(); });
                 entries[i].make_window(context_window, 0, y, _width, _height);
                 entries[i].window.emit_WIN_SIG(DRAW_SIGNAL);
-                entries[i].window.emit_signal_on_ev(L_MOUSE_BUTTON_PRESS);
+                // entries[i].window.emit_signal_on_ev(L_MOUSE_BUTTON_PRESS);
             }
         }
     
@@ -6848,10 +6848,7 @@ class context_menu {
                         }
                     }
 
-                    if (context_window.is_mapped())
-                    {
-                        hide__();
-                    }
+                    hide__();
                 }
             });
 
