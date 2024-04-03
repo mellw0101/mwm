d5229301ef314e87ca05e0a2d1fb3ed423a658e8
mellw0101 <melwivensson@gmail.com>
2024-04-03 11:04:33 +0000

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7225,16 +7225,32 @@ class client {
             CWC(frame);
 
             CONN(DESTROY_NOTIFY, if (__window == this->win) {
-                if (!this->win.is_mapped()) {
-                    this->kill();
+                if (this->win.is_mapped()) {
+                    loutI << "this->win is mapped so killed win" << loutEND;
+                    this->win.kill();
 
                 }
                 else {
-                    this->win.kill();
+                    loutI << "this->win is not mapped so killed client" << loutEND;
+                    this->kill();
 
                 }
 
             }, this->win);
+
+            CONN(DESTROY_NOTIFY, if (__window == this->frame) {
+                if (this->win.is_mapped()) {
+                    loutI << "from this->frame this->win is mapped so killed win" << loutEND;
+                    this->win.kill();
+
+                }
+                else {
+                    loutI << "from this->frane this->win is not mapped so killed client" << loutEND;
+                    this->kill();
+
+                }
+
+            }, this->frame);
 
         }    
         void make_titlebar() {
