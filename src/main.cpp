c052103caf634506421cd94c0743c034441dfbf4
mellw0101 <melwivensson@gmail.com>
2024-04-05 10:01:10 +0000

    Changes to be committed: 	modified:   src/main.cpp 	modified:   src/xcb.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -94,7 +94,7 @@ static const xcb_setup_t * setup;
 static xcb_screen_iterator_t iter;
 static xcb_screen_t * screen;
 
-#include "xcb.hpp"
+// #include "xcb.hpp"
 
 #define DEFAULT_FONT "7x14"
 #define DEFAULT_FONT_WIDTH 7
@@ -943,6 +943,59 @@ namespace {
 
             }
     };
+
+}
+namespace xcb {
+    xcb_intern_atom_cookie_t intern_atom_cookie(xcb_connection_t *__c, const char *__name) {
+        return xcb_intern_atom(
+            __c,
+            0,
+            slen(__name),
+            __name
+            
+        );
+
+    }
+    xcb_intern_atom_reply_t *intern_atom_reply(xcb_connection_t *__c, xcb_intern_atom_cookie_t __cookie) {
+        return xcb_intern_atom_reply(__c, __cookie, nullptr);
+
+    }
+    xcb_atom_t intern_atom(xcb_connection_t *__c, const char *__name) {
+        xcb_intern_atom_reply_t *rep = intern_atom_reply(__c, intern_atom_cookie(__c, __name));
+        if (!rep) {
+            loutE << "rep = nullptr could not get intern_atom_reply" << loutEND;
+            return XCB_ATOM_NONE;
+        
+        } xcb_atom_t atom(rep->atom);
+        free(rep);
+        return atom;
+
+    }
+    bool window_exists(xcb_connection_t *__c, uint32_t __w) {
+        xcb_generic_error_t *err;
+        free(xcb_query_tree_reply(__c, xcb_query_tree(__c, __w), &err));
+        if (err != NULL) {
+            free(err);
+            return false;
+
+        }
+        return true;
+
+    }
+    uint32_t gen_Xid() {
+        uint32_t w = xcb_generate_id(conn);
+        return (window_exists(conn, w)) ? w : XCB_NONE;
+
+    }
+    void window_stack(xcb_connection_t *__c, uint32_t __window1, uint32_t __window2, uint32_t __mode) {
+        if (__window2 == XCB_NONE) return;
+        
+        uint16_t mask = XCB_CONFIG_WINDOW_SIBLING | XCB_CONFIG_WINDOW_STACK_MODE;
+        uint32_t values[] = {__window2, __mode};
+        
+        xcb_configure_window(__c, __window1, mask, values);
+        
+    }
 
 }
 namespace XCB {
--- src/xcb.cpp
+++ src/xcb.cpp
@@ -1,64 +1,64 @@
-// #include <cstddef>
-#include <cstdint>
-#include "Log.hpp"
-#include <xcb/xcb.h>
-#include <xcb/xproto.h>
-#include <xcb/xcb_ewmh.h>
-#include "tools.hpp"
-#include "xcb.hpp"
+// // #include <cstddef>
+// #include <cstdint>
+// #include "Log.hpp"
+// #include <xcb/xcb.h>
+// #include <xcb/xproto.h>
+// #include <xcb/xcb_ewmh.h>
+// #include "tools.hpp"
+// #include "xcb.hpp"
 
-xcb_connection_t *conn = nullptr;
+// xcb_connection_t *conn = nullptr;
 
-namespace xcb {
-    xcb_intern_atom_cookie_t intern_atom_cookie(xcb_connection_t *__c, const char *__name) {
-        return xcb_intern_atom(
-            __c,
-            0,
-            slen(__name),
-            __name
+// namespace xcb {
+//     xcb_intern_atom_cookie_t intern_atom_cookie(xcb_connection_t *__c, const char *__name) {
+//         return xcb_intern_atom(
+//             __c,
+//             0,
+//             slen(__name),
+//             __name
             
-        );
+//         );
 
-    }
-    xcb_intern_atom_reply_t *intern_atom_reply(xcb_connection_t *__c, xcb_intern_atom_cookie_t __cookie) {
-        return xcb_intern_atom_reply(__c, __cookie, nullptr);
+//     }
+//     xcb_intern_atom_reply_t *intern_atom_reply(xcb_connection_t *__c, xcb_intern_atom_cookie_t __cookie) {
+//         return xcb_intern_atom_reply(__c, __cookie, nullptr);
 
-    }
-    xcb_atom_t intern_atom(xcb_connection_t *__c, const char *__name) {
-        xcb_intern_atom_reply_t *rep = intern_atom_reply(__c, intern_atom_cookie(__c, __name));
-        if (!rep) {
-            loutE << "rep = nullptr could not get intern_atom_reply" << loutEND;
-            return XCB_ATOM_NONE;
+//     }
+//     xcb_atom_t intern_atom(xcb_connection_t *__c, const char *__name) {
+//         xcb_intern_atom_reply_t *rep = intern_atom_reply(__c, intern_atom_cookie(__c, __name));
+//         if (!rep) {
+//             loutE << "rep = nullptr could not get intern_atom_reply" << loutEND;
+//             return XCB_ATOM_NONE;
         
-        } xcb_atom_t atom(rep->atom);
-        free(rep);
-        return atom;
+//         } xcb_atom_t atom(rep->atom);
+//         free(rep);
+//         return atom;
 
-    }
-    bool window_exists(xcb_connection_t *__c, uint32_t __w) {
-        xcb_generic_error_t *err;
-        free(xcb_query_tree_reply(__c, xcb_query_tree(__c, __w), &err));
-        if (err != NULL) {
-            free(err);
-            return false;
+//     }
+//     bool window_exists(xcb_connection_t *__c, uint32_t __w) {
+//         xcb_generic_error_t *err;
+//         free(xcb_query_tree_reply(__c, xcb_query_tree(__c, __w), &err));
+//         if (err != NULL) {
+//             free(err);
+//             return false;
 
-        }
-        return true;
+//         }
+//         return true;
 
-    }
-    uint32_t gen_Xid() {
-        uint32_t w = xcb_generate_id(conn);
-        return (window_exists(conn, w)) ? w : XCB_NONE;
+//     }
+//     uint32_t gen_Xid() {
+//         uint32_t w = xcb_generate_id(conn);
+//         return (window_exists(conn, w)) ? w : XCB_NONE;
 
-    }
-    void window_stack(xcb_connection_t *__c, uint32_t __window1, uint32_t __window2, uint32_t __mode) {
-        if (__window2 == XCB_NONE) return;
+//     }
+//     void window_stack(xcb_connection_t *__c, uint32_t __window1, uint32_t __window2, uint32_t __mode) {
+//         if (__window2 == XCB_NONE) return;
         
-        uint16_t mask = XCB_CONFIG_WINDOW_SIBLING | XCB_CONFIG_WINDOW_STACK_MODE;
-        uint32_t values[] = {__window2, __mode};
+//         uint16_t mask = XCB_CONFIG_WINDOW_SIBLING | XCB_CONFIG_WINDOW_STACK_MODE;
+//         uint32_t values[] = {__window2, __mode};
         
-        xcb_configure_window(__c, __window1, mask, values);
+//         xcb_configure_window(__c, __window1, mask, values);
         
-    }
+//     }
 
-}
+// }
