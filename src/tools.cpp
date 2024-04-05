854b9da7494b5e5aa3d4e949e9c07cc2e50ccdf6
mellw0101 <melwivensson@gmail.com>
2024-04-05 08:06:48 +0000

    Changes to be committed: 	modified:   src/tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -89,9 +89,13 @@ namespace /* slen */ {
 * @p '__s'
 * @brief function that @return's len of a 'const char *'
 */
+// size_t slen(const char *__s) {
+//     size_t i(0);
+//     for(; __s[i]; ++i){}
+//     return i;
+    
+// }
 size_t slen(const char *__s) {
-    size_t i(0);
-    for(; __s[i]; ++i){}
-    return i;
+    for (;;) if (!*++__s) return __s - &__s[0];
     
 }
