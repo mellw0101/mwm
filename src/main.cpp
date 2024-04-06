2e98a0722d773bdafa8def9fbffaeeda00c744d3
mellw0101 <melwivensson@gmail.com>
2024-04-06 22:55:39 +0000

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3072,14 +3072,14 @@ class __event_handler__ {
                 HANDLE(XCB_MAP_REQUEST, e->window);
 
             });
-            setEventCallback(XCB_MOTION_NOTIFY, [&](Ev ev) {
-                RE_CAST_EV(xcb_motion_notify_event_t);
-                thread_pool.enqueue([](uint32_t w) {
-                    Emit(w, XCB_MOTION_NOTIFY);
+            // setEventCallback(XCB_MOTION_NOTIFY, [&](Ev ev) {
+            //     RE_CAST_EV(xcb_motion_notify_event_t);
+            //     thread_pool.enqueue([](uint32_t w) {
+            //         Emit(w, XCB_MOTION_NOTIFY);
                     
-                }, e->event);
+            //     }, e->event);
 
-            });
+            // });
             setEventCallback(XCB_BUTTON_PRESS, [&](Ev ev) {
                 RE_CAST_EV(xcb_button_press_event_t);
                 if (e->detail == L_MOUSE_BUTTON)        {
